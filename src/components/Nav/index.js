3f2cac1f96ec3c03b91511cdffa3a564780d2eaf
Nicholas Nolen <nicnolen@ymail.com>
2022-03-21 10:30:20 -0500

    added categories and an event listener for when a category is clicked in the Nav component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,25 +3,52 @@ import React from 'react';
 
 // Create the `Nav` component
 const Nav = () => {
-  return <header>
-    <h2>
-      <a href='/'>
-        <span role="img" aria-label="camera">📸</span> Oh Snap!
-      </a>
-    </h2>
-    <nav>
-      <ul className="flex-row">
-        <li className='mx-2'>
-          <a href='#about'>
-            About me
-          </a>
-        </li>
-        <li>
-          <span>Contact</span>
-        </li>
-      </ul>
-    </nav>
-  </header>;
+  const categories = [
+    {
+      name: 'commercial',
+      description:
+        'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    {
+      name: 'landscape',
+      description: 'Fields, farmhouses, waterfalls, and the beauty of nature',
+    },
+  ];
+
+  const categorySelected = () => {
+    console.log('hello');
+  };
+
+  return (
+    <header>
+      <h2>
+        <a href='/'>
+          <span role='img' aria-label='camera'>
+            📸
+          </span>
+          Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className='flex-row'>
+          <li className='mx-2'>
+            <a href='#about'>About me</a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+          {categories.map(category => (
+            // whenever you map over anything in JSX the outmost element must have a key attribute set to something unique
+            <li className='mx-1' key={category.name}>
+              <span onClick={categorySelected}>{category.name}</span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 // Export the module
