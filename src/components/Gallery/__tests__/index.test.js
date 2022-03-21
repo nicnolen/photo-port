23bc399992a444c13294eb403d4d7173220f592a
Nicholas Nolen <nicnolen@ymail.com>
2022-03-21 15:38:21 -0500

    added initial test for the Gallery component with baseline test

---
+++ src/components/Gallery/__tests__/index.test.js
@@ -0,0 +1,22 @@
+// Import dependencies
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Gallery from '..';
+
+// Mock props
+const portrait = {
+  name: 'portraits',
+  description: 'Portraits of people in my life',
+};
+
+// After each test, make sure there isnt any leftover memory that could give you false results
+afterEach(cleanup);
+
+// Declare the components you are testing
+describe('Gallery is rendering', () => {
+  // baseline test
+  it('renders', () => {
+    render(<Gallery currentCategory={portrait} />);
+  });
+});
