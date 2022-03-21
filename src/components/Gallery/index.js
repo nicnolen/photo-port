4601d60fcc6f24b37d9a834d7a8fcc2baec58731
Nicholas Nolen <nicnolen@ymail.com>
2022-03-21 13:22:50 -0500

    updated the Galary component so it gets its name and description from an object

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,12 +1,19 @@
 // Import dependencies
 import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 // Create the `Gallery` function
-const Gallery = () => {
+const Gallery = (props) => {
+  const currentCategory = {
+    name: 'commercial',
+    description:
+      'Photos of grocery stores, food trucks, and other commercial projects',
+  };
+
   return (
     <section>
-      <h1>NAME</h1>
-      <p>DESCRIPTION</p>
+      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
     </section>
   );
 };
