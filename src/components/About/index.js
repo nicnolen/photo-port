f80033a14cf1536db28b8333170428c774bad6ca
Nicholas Nolen <nicnolen@ymail.com>
2022-03-21 09:44:23 -0500

    created an initial About component and tested to make sure it rendered in the browser

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+// Import React
+import React from 'react';
+
+// Create the `About` function
+const About = () => {
+  return (
+    <section>
+      <h1 id='about'>Who am I?</h1>
+    </section>
+  );
+};
+
+// Export components
+export default About;
