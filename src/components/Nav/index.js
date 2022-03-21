86319358210f9f139508508cfe648e3e43ad689d
Nicholas Nolen <nicnolen@ymail.com>
2022-03-21 10:06:32 -0500

    added a logo to the Nav component as well as the About and Contact sections

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,7 +3,25 @@ import React from 'react';
 
 // Create the `Nav` component
 const Nav = () => {
-  return <header></header>;
+  return <header>
+    <h2>
+      <a href='/'>
+        <span role="img" aria-label="camera">📸</span> Oh Snap!
+      </a>
+    </h2>
+    <nav>
+      <ul className="flex-row">
+        <li className='mx-2'>
+          <a href='#about'>
+            About me
+          </a>
+        </li>
+        <li>
+          <span>Contact</span>
+        </li>
+      </ul>
+    </nav>
+  </header>;
 };
 
 // Export the module
