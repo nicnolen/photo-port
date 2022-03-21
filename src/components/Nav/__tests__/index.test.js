21686002f01abe3494b13405e88545ba3d035678
Nicholas Nolen <nicnolen@ymail.com>
2022-03-21 11:55:19 -0500

    imported dependencies and configured the testing environment for the Nav component

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,15 @@
+// Import dependencies
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+// After each test, make sure there isnt any leftover memory that could give you false results
+afterEach(cleanup);
+
+// Declare the components you are testing
+describe('Nav component', () => {
+  // baseline test
+
+  // snapshot test
+})
