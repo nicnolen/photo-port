37bf2cbab66045d9f58de1bc7cb96197ef8854f3
Nicholas Nolen <nicnolen@ymail.com>
2022-03-23 09:26:29 -0500

    initialized the state to clear input fields in the user interface

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,30 @@
 // Import dependencies
-import React from 'react';
+import React, { useState } from 'react';
 
 // Make component function
 function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const { name, email, message } = formState;
+
   return (
     <section>
       <h1>Contact me</h1>
       <form id='contact-form'>
         <div>
           <label htmlFor='name'>Name:</label>
-          <input type='text' name='name' />
+          <input type='text' name='name' defaultValue={name} />
         </div>
         <div>
           <label htmlFor='email'>Email address:</label>
-          <input type='email' name='email' />
+          <input type='email' name='email' defaultValue={email} />
         </div>
         <div>
           <label htmlFor='message'>Message:</label>
-          <textarea name='message' rows='5' />
+          <textarea name='message' rows='5' defaultValue={message} />
         </div>
         <button type='submit'>Submit</button>
       </form>
