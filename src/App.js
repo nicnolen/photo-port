6577187e31354c0a0e2a9c1bcf816a772d16ab34
Nicholas Nolen <nicnolen@ymail.com>
2022-03-23 08:55:04 -0500

    added contact form to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -27,10 +28,9 @@ function App() {
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}></Nav>
       <main>
-        <div>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+        <ContactForm></ContactForm>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
